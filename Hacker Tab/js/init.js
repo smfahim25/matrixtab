bfd22553cac0c5af2344ab7634a8c41421715fa4
S M Fahim Hossen <96822713+smfahim25@users.noreply.github.com>
2022-03-29 21:01:16 +0600

    added

---
+++ Hacker Tab/js/init.js
@@ -0,0 +1,31 @@
+document.getElementById("searchBar").addEventListener("focus", function(){addClass('mainContainer','input-active')});
+document.getElementById("searchBar").addEventListener("blur", function(){removeClass('mainContainer','input-active')});
+document.getElementById("searchBar").addEventListener("keydown", function(){handleQuery(event,this.value)});
+$('#customAnimationsBtn').click(function(e){
+    toggleAnimations();
+});
+$('#closebutton').click(function(e){
+    e.preventDefault();
+    closeNav();
+});
+$('#sidenavhomea').click(function(e){
+    e.preventDefault();
+    openNav();
+});
+init();
+$( "#leftsidemenu" ).mousemove(function( event ) {
+    var msg = "Handler for .mousemove() called at ";
+    msg += event.pageX + ", " + event.pageY;
+    console.log(msg);
+    if(event.pageX<100){
+        openNav()
+    }
+});
+setTimeout(function() {
+    $("#customMatrix").on('DOMSubtreeModified', "#colorPicker", function() {
+        updateMatrixColor($('#colorPicker').html());
+    });
+}, 1000);
+$('#ip').click(function(){
+    toggleShowIP()
+});
